137fc71f5356c1bd9323c2190014cf0716713966
Kamil Jozwik <kamil.jozwik@britishcouncil.org>
2021-07-02 21:12:40 +0200

    init commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useState, useEffect } from 'react';
 
-export default function Home() {
+import styles from "../styles/Home.module.css";
+
+export default function Home(props) {
+
+  const [fetched, setFetched] = useState()
+
+  useEffect(() => {
+    fetch("http://ipwhois.app/json/")
+    .then((response) => response.json())
+    .then((json) => setFetched(json))
+    .catch((e) => console.log(e));
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
@@ -9,57 +21,28 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main className={styles.main}>Hello</main>
+      <pre>
+        <p>Fetched on client</p>
+        {JSON.stringify(fetched, null, 4)}
+      </pre>
+      <pre>
+        <p>from server props</p>
+        {JSON.stringify(props.data, null, 4)}
+      </pre>
+      <h3>{props.ip}</h3>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
-  )
+  );
+}
+
+export async function getServerSideProps(context) {
+  const res = await fetch("http://ipwhois.app/json/");
+  const data = await res.json();
+
+  const ip = context.req.socket.localAddress;
+
+  // Pass data to the page via props
+  return { props: { data, ip } };
 }
